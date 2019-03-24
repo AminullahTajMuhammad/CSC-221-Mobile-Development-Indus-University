6e8a345b1b993cea7cd24ece76ecf0bb5db90c5e
amin <aminullahtajmuhammad@gmail.com>
2019-03-24 15:33:12 +0500

    Update Lab 2

---
+++ app/src/main/java/com/amin/indusuniversity/LabManual_2/LabManual_Two.java
@@ -0,0 +1,29 @@
+package com.amin.indusuniversity.LabManual_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.amin.indusuniversity.R;
+
+public class LabManual_Two extends AppCompatActivity {
+
+    TextView tvHelloAndroid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab_manual_two_activity);
+
+        tvHelloAndroid = findViewById(R.id.tvHelloAndroid);
+        tvHelloAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvHelloAndroid.setTextColor(getResources().getColor(android.R.color.holo_blue_dark));
+                tvHelloAndroid.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+            }
+        });
+
+    }
+}
