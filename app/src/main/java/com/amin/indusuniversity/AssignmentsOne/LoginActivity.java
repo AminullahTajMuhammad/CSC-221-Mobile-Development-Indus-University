baef090b562d775226c9ec1dcad94a22dca503e1
amin <aminullahtajmuhammad@gmail.com>
2019-04-03 01:20:18 +0500

    Working on Assignment

---
+++ app/src/main/java/com/amin/indusuniversity/AssignmentsOne/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.amin.indusuniversity.AssignmentsOne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amin.indusuniversity.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
